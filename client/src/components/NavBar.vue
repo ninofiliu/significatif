66e96c3f8d5f5f6fa6799c911e9cbe64f4601ca4
ninofiliu <nino.filiu@gmail.com>
2020-07-21 21:02:00 +0200

    component: NavBar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="NavBar">
+    <router-link to="/">
+      significa.tif
+    </router-link>
+    <div>
+      <router-link to="/photo">photo</router-link>
+      <router-link to="/video">video</router-link>
+      <router-link to="/manifesto">manifesto</router-link>
+      <router-link to="/about">about</router-link>
+      <a href="#" class="close">&Cross;</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+.NavBar {
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  border-bottom: 1px solid var(--red);
+  padding: 22px 36px;
+  display: flex;
+  justify-content: space-between;
+
+  font-size: 24px;
+  text-transform: uppercase;
+}
+a {
+  margin: 0 16px;
+}
+</style>>
