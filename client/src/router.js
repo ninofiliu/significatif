368296bb34f830197dd50830986f27c657831b75
ninofiliu <nino.filiu@gmail.com>
2020-07-28 18:24:14 +0200

    add route

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import About from './views/about/About.vue';
+import Archive from './views/archive/Archive.vue';
 import Home from './views/home/Home.vue';
 import Manifesto from './views/manifesto/Manifesto.vue';
 
@@ -10,8 +11,9 @@ const router = new VueRouter({
   mode: 'hash',
   routes: [
     { path: '/', component: Home },
-    { path: '/manifesto', component: Manifesto },
     { path: '/about', component: About },
+    { path: '/archive', component: Archive },
+    { path: '/manifesto', component: Manifesto },
     { path: '*', redirect: '/' },
   ],
 });
---
+++ client/src/views/archive/Archive.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="Archive"/>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
