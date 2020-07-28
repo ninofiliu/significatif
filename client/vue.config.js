57d74a0cce5b233a7e274bc837b31368a49b6e4e
ninofiliu <nino.filiu@gmail.com>
2020-07-28 18:29:43 +0200

    fix build

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,17 @@
-module.exports = {
-  outputDir: 'dist-future',
-  publicPath: '/future/',
-};
+module.exports = (() => {
+  switch (process.env.NODE_ENV) {
+    case 'future':
+      return {
+        outputDir: 'dist-future',
+        publicPath: '/future/',
+      };
+    case 'prod':
+      return {
+        outputDir: 'dist-prod',
+      };
+    case 'dev':
+      return {};
+    default:
+      throw new Error(`Unknown NODE_ENV "${process.env.NODE_ENV}"`);
+  }
+})();
