1a4e0ec40479aaff08dc862fcc2f12c28b1a4747
ninofiliu <nino.filiu@gmail.com>
2020-08-05 13:30:00 +0200

    pictures component

--- client/src/views/photos/Photos.vue
+++ client/src/views/photos/Photos.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="Photos">
     <Article/>
+    <Pictures/>
     <NavBar/>
     <Footer fixed/>
   </div>
@@ -10,12 +11,14 @@
 import Footer from '../../components/Footer.vue';
 import NavBar from '../../components/NavBar.vue';
 import Article from './Article.vue';
+import Pictures from './Pictures.vue';
 
 export default {
   components: {
     Article,
     Footer,
     NavBar,
+    Pictures,
   },
 };
 </script>
---
+++ client/src/views/photos/Pictures.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Pictures">
+    <img src="//placekitten.com/300/460">
+    <img src="//placekitten.com/300/461">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.Pictures {
+  position: fixed;
+  top: 0;
+  right: 60%;
+  bottom: 0;
+  left: 0;
+  position: relative;
+}
+img {
+  left: 50%;
+  right: 50%;
+  transform: translate(-50%, -50%) rotate(-20deg);
+}
+</style>
