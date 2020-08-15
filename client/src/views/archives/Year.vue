9083ea81e669958a654f9877f74755fb6600cb50
ninofiliu <nino.filiu@gmail.com>
2020-08-15 18:49:30 +0200

    progressive circle around the year

--- client/src/views/archives/Year.vue
+++ client/src/views/archives/Year.vue
@@ -1,8 +1,10 @@
 <template>
-  <svg width="240" height="110" :class="{ '--current': current }">
+  <svg width="240" height="110" @click="$emit('click')">
     <text x="120" y="55" text-anchor="middle" dominant-baseline="middle">
       {{ year }}
     </text>
+    <path :d="path0D" />
+    <path v-if="progress > 0.5" :d="path1D" />
   </svg>
 </template>
 
@@ -16,6 +18,31 @@ export default {
     current: {
       required: true,
       type: Boolean,
+    },
+  },
+  data() {
+    return {
+      progress: 0,
+      interval: setInterval(() => {
+        if (this.current && this.progress < 1) this.progress += 1 / 32;
+        if (!this.current && this.progress > 0) this.progress -= 1 / 32;
+      }, 1000 / 30),
+    };
+  },
+  computed: {
+    endX() {
+      return 120 + 115 * Math.sin(2 * Math.PI * this.progress);
+    },
+    endY() {
+      return 55 - 50 * Math.cos(2 * Math.PI * this.progress);
+    },
+    path0D() {
+      return this.progress < 0.5
+        ? `M 120 5 A 115 50 0 0 1 ${this.endX} ${this.endY}`
+        : 'M 120 5 A 115 50 0 0 1 120 105';
+    },
+    path1D() {
+      return `M 120 105 A 115 50 0 0 1 ${this.endX} ${this.endY}`;
     },
   },
 };
@@ -33,5 +60,10 @@ text {
   font-size: 70px;
   fill: var(--red);
   font-weight: 300;
+}
+path {
+  stroke: var(--red);
+  stroke-width: 2;
+  fill: none;
 }
 </style>
