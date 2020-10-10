cb90efe429e588ebe7e97a10b5d343add3d5af72
ninofiliu <nino.filiu@gmail.com>
2020-10-10 19:13:20 +0200

    /photos - add text

--- client/src/views/photos/Photos.vue
+++ client/src/views/photos/Photos.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <div class="text">
+      <span style="font-weight: bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </span>
+      <span>
+        In sequi similique tenetur facere ratione sapien tenatus dolor illum nesciunt dolorum
+      </span>
+      <span class="font-ogg-roman">
+        non at ab laboriosam pariatur quaerat dolores maxime!
+      </span>
+      <span>
+        impedit tempora numquam necessitatibus corrupti
+      </span>
+      <img :src="`${$s}6.jpg`" class="text-img">
+      <span>
+        numquam necessitatibus cupiditate?
+      </span>
+    </div>
     <NavBar/>
     <Footer fixed/>
   </div>
@@ -16,3 +34,13 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.text {
+  position: absolute;
+  top: 20%;
+  right: 40%;
+  left: 10%;
+  font-size: 30px;
+}
+</style>
