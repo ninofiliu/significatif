ecf00918477331c7b3ac680236f5a5dc302e40c2
Nino Filiu <nino.filiu@gmail.com>
2020-12-23 22:33:53 +0100

    [fs] : extract Scroll

--- client/src/views/home/Home.vue
+++ client/src/views/home/Home.vue
@@ -1,17 +1,7 @@
 <template>
   <div>
     <NavBar/>
-    <div class="scroll">
-      <div class="scroll-content">
-        <div
-          v-for="src of srcs"
-          :key="src"
-          class="img-container"
-        >
-          <img :src="`${$s}${src}`">
-        </div>
-      </div>
-    </div>
+    <Scroll/>
     <Center/>
     <Footer fixed/>
   </div>
@@ -20,54 +10,15 @@
 <script>
 import NavBar from '../../components/NavBar.vue';
 import Center from './Center.vue';
+import Scroll from './Scroll.vue';
 import Footer from '../../components/Footer.vue';
-import srcs from '../../content/home.json';
 
 export default {
   components: {
     Center,
     Footer,
     NavBar,
-  },
-  data() {
-    return {
-      srcs,
-    };
+    Scroll,
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.scroll {
-  position: fixed;
-  top: 3rem;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  overflow-x: auto;
-  overflow-y: hidden;
-}
-.scroll-content {
-  display: flex;
-  width: calc(30vw * 8);
-}
-.img-container {
-  box-sizing: border-box;
-  width: 30vw;
-  height: calc(100vh - 3rem);
-  padding: 3vw;
-  display: flex;
-  &:nth-child(4n+1) {
-    align-items: flex-end;
-  }
-  &:nth-child(2n) {
-    align-items: center;
-  }
-  &:nth-child(4n+3) {
-    align-items: flex-start;
-  }
-}
-img {
-  width: 100%;
-}
-</style>
---
+++ client/src/views/home/Scroll.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="scroll">
+    <div class="scroll-content">
+      <div
+        v-for="src of srcs"
+        :key="src"
+        class="img-container"
+      >
+        <img :src="`${$s}${src}`">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import srcs from '../../content/home.json';
+
+export default {
+  data() {
+    return {
+      srcs,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.scroll {
+  position: fixed;
+  top: 3rem;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+.scroll-content {
+  display: flex;
+  width: calc(30vw * 8);
+}
+.img-container {
+  box-sizing: border-box;
+  width: 30vw;
+  height: calc(100vh - 3rem);
+  padding: 3vw;
+  display: flex;
+  &:nth-child(4n+1) {
+    align-items: flex-end;
+  }
+  &:nth-child(2n) {
+    align-items: center;
+  }
+  &:nth-child(4n+3) {
+    align-items: flex-start;
+  }
+}
+img {
+  width: 100%;
+}
+</style>
