dcf8de58fad2e1ee6e2a8066a94c40842ee54e9d
Nino Filiu <nino.filiu@gmail.com>
2020-12-23 23:13:28 +0100

    home : scroll

--- client/src/views/home/Scroll.vue
+++ client/src/views/home/Scroll.vue
@@ -1,60 +1,53 @@
 <template>
-  <div class="scroll">
-    <div class="scroll-content">
-      <div
-        v-for="src of srcs"
-        :key="src"
-        class="img-container"
-      >
-        <img :src="`${$s}${src}`">
-      </div>
-    </div>
+  <div>
+    <img
+      v-for="(picture, index) of pictures"
+      :key="picture.src"
+      :src="`${$s}${picture.src}`"
+      :style="{
+        left: `${(40 * index) - ((0.5 + 0.5 * picture.speed) * scrolled)}vw`,
+        top: `${30 + 50 * picture.y}vh`,
+      }"
+    />
   </div>
 </template>
 
 <script>
 import srcs from '../../content/home.json';
 
+const pictures = srcs.map((src) => ({
+  y: Math.random(),
+  speed: Math.random(),
+  src,
+}));
+
 export default {
   data() {
     return {
-      srcs,
+      pictures,
+      scrolled: 0,
     };
+  },
+  mounted() {
+    window.addEventListener('wheel', this.onWheel);
+  },
+  beforeDestroy() {
+    window.removeEventListener('wheel', this.onwheel);
+  },
+  methods: {
+    onWheel(evt) {
+      this.scrolled += evt.deltaY;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.scroll {
-  position: fixed;
-  top: 3rem;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  overflow-x: auto;
-  overflow-y: hidden;
-}
-.scroll-content {
-  display: flex;
-  width: calc(30vw * 8);
-}
-.img-container {
-  box-sizing: border-box;
-  width: 30vw;
-  height: calc(100vh - 3rem);
-  padding: 3vw;
-  display: flex;
-  &:nth-child(4n+1) {
-    align-items: flex-end;
-  }
-  &:nth-child(2n) {
-    align-items: center;
-  }
-  &:nth-child(4n+3) {
-    align-items: flex-start;
-  }
-}
 img {
-  width: 100%;
+  position: fixed;
+  pointer-events: none;
+  transition: all 0.5s;
+  height: 45vh;
+  transform: translate(-50%, -50%);
 }
 </style>
