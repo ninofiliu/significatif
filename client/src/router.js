8c7ed27d3595b3124fa51cfa57f6be5101b011d1
Nino Filiu <nino.filiu@gmail.com>
2021-03-17 21:34:05 +0100

    /photos/humans : rework

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import ArchitectureSilence from './views/photos/ArchitectureSilence.vue';
 import Archives from './views/archives/Archives.vue';
 import Contact from './views/contact/Contact.vue';
-import CrossTheBorders from './views/photos/CrossTheBorders.vue';
 import Home from './views/home/Home.vue';
 import Manifesto from './views/manifesto/Manifesto.vue';
-import PedalToTheMetal from './views/photos/PedalToTheMetal.vue';
-import PhotoSeduction from './views/photos/PhotoSeduction.vue';
+import Humans from './views/photos/Humans.vue';
 import Photos from './views/photos/Photos.vue';
-import TasteOfFarniente from './views/photos/TasteOfFarniente.vue';
-import VousEtesDesAnimaux from './views/photos/VousEtesDesAnimaux.vue';
 
 Vue.use(VueRouter);
 
@@ -23,12 +18,7 @@ const router = new VueRouter({
     { path: '/archives', component: Archives },
     { path: '/manifesto', component: Manifesto },
     { path: '/photos', component: Photos },
-    { path: '/photos/silent-photography', component: ArchitectureSilence },
-    { path: '/photos/cross-the-borders', component: CrossTheBorders },
-    { path: '/photos/pedal-to-the-metal', component: PedalToTheMetal },
-    { path: '/photos/humans', component: PhotoSeduction },
-    { path: '/photos/taste-of-farniente', component: TasteOfFarniente },
-    { path: '/photos/vous-etes-des-animaux', component: VousEtesDesAnimaux },
+    { path: '/photos/humans', component: Humans },
     { path: '*', redirect: '/' },
   ],
 });
---
+++ client/src/views/photos/Humans.vue
@@ -0,0 +1,83 @@
+<template>
+  <Story
+    id="humans"
+    color="var(--purple)"
+    :backgroundPosition="{ }"
+    :picturesPosition="{ top: '0', right: '0', bottom: '35vh', left: '50vw' }"
+    picturesMode="horizontal"
+  >
+    <div class="bubble u-appear-scaleup"/>
+    <div class="humans">
+      <AppearingLine :chars="humans"/>
+    </div>
+    <div class="text-container">
+      <p v-for="t of txt" :key="t">{{ t }}</p>
+    </div>
+  </Story>
+</template>
+
+<script>
+import AppearingLine from '../../components/AppearingLine.vue';
+import Story from '../../components/story/Story.vue';
+import covers from '../../content/covers.json';
+
+export default {
+  components: {
+    AppearingLine,
+    Story,
+  },
+  data() {
+    return {
+      txt: covers.humans.txt,
+      humans: [
+        { html: 'H', class: { 'u-font-ogg': true, 'u-italic': true } },
+        { html: 'u', class: { 'u-font-ogg': true, 'u-italic': true } },
+        { html: 'm', class: { 'u-font-ogg': true, 'u-italic': true } },
+        { html: 'a', class: { 'u-font-ogg': true, 'u-italic': true } },
+        { html: 'n', class: { 'u-font-ogg': true, 'u-italic': true } },
+        { html: 's', class: { } },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bubble {
+  position: fixed;
+  top: 18vh;
+  left: 12vw;
+  width: 25vw;
+  height: 25vw;
+  background-color: white;
+  border-radius: 50%;
+}
+.humans {
+  position: fixed;
+  top: 20vh;
+  left: 10vw;
+  font-size: 4.5rem;
+  font-weight: 300;
+}
+.text-container {
+  position: fixed;
+  top: 45vh;
+  right: 55vw;
+  left: 15vw;
+  font-size: .8rem;
+}
+.text {
+  animation-name: appear-up;
+  animation-fill-mode: both;
+  animation-delay: 1.3s;
+  animation-duration: 1s;
+}
+@keyframes appear-up {
+  from {
+    transform: translateY(40vh);
+  }
+  to {
+    transform: translateY(0%)s;
+  }
+}
+</style>
