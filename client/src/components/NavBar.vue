679675cd0a7b397db91c89b4979318d20d5af65a
Nino Filiu <nino.filiu@gmail.com>
2021-05-20 21:50:03 +0200

    mobile navbar and menu

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="NavBar">
-    <RouterLink to="/">
-      significa.tif
-    </RouterLink>
-    <div>
-      <RouterLink to="/photos">photos</RouterLink>
-      <RouterLink to="/manifesto">manifesto</RouterLink>
-      <RouterLink to="/contact">contact</RouterLink>
+  <div>
+    <div class="navbar --desktop">
+      <RouterLink to="/">
+        significa.tif
+      </RouterLink>
+      <div>
+        <RouterLink to="/photos">photos</RouterLink>
+        <RouterLink to="/manifesto">manifesto</RouterLink>
+        <RouterLink to="/contact">contact</RouterLink>
+      </div>
+    </div>
+    <div class="navbar --mobile">
+      <RouterLink to="/">significa.tif</RouterLink>
+      <button @click="closed = !closed">☰</button>
+      <div class="menu" :class="{ '--closed': closed }">
+        <RouterLink to="/photos">photos</RouterLink>
+        <RouterLink to="/manifesto">manifesto</RouterLink>
+        <RouterLink to="/contact">contact</RouterLink>
+        <RouterLink to="/archives">archives</RouterLink>
+        <RouterLink to="/archives">archives</RouterLink>
+        <a href="https://www.instagram.com/victorair_/" target="_blank" rel="noreferrer noopener">instagram</a>
+      </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      closed: true,
+    };
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.NavBar {
+.navbar {
   z-index: 100;
   position: fixed;
   top: 0;
@@ -28,9 +52,6 @@
 
   font-size: 1rem;
   text-transform: uppercase;
-  > div a {
-    margin-left: 2rem;
-  }
 }
 a {
   vertical-align: middle;
@@ -40,5 +61,50 @@ a {
 }
 .router-link-exact-active:not(.close) {
   text-decoration: underline;
+}
+
+.--desktop {
+  > div a {
+    margin-left: 2rem;
+  }
+}
+.--mobile {
+  display: none;
+}
+@media screen and (max-width: 900px) {
+  .--desktop {
+    display: none;
+  }
+  .--mobile {
+    display: flex;
+  }
+  button {
+    border: none;
+    color: inherit;
+    font: inherit;
+    width: 5vw;
+    padding: 0;
+  }
+  .menu {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0vw;
+    right: 15vw;
+    background: var(--red);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    transition: all .5s;
+    &.--closed {
+      transform: translateX(-100%);
+    }
+    a {
+      height: 3rem;
+      display: flex;
+      align-items: center;
+      padding: 0 5vw;
+    }
+  }
 }
 </style>>
