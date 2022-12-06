aa2cc2033795b4f2aaaa4cbebd00f0d226a546b6
Nino Filiu <nino.filiu@gmail.com>
2022-12-06 23:59:07 +0100

    fix static base

--- client/src/globals.ts
+++ client/src/globals.ts
@@ -1,2 +1,5 @@
 export const ROUTER_BASE = "/";
-export const STATIC_BASE = "http://localhost:3001/";
+export const STATIC_BASE =
+  location.hostname === "significatif.org"
+    ? "https://storage.googleapis.com/significatif/"
+    : `http://${location.hostname}:3001/`;
