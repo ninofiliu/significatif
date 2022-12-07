925273891f80a6b694882151d7cdf2c3c3be059c
Nino Filiu <nino.filiu@gmail.com>
2022-12-07 17:56:31 +0100

    home mobile scroll + loop

--- client/src/views/home/Scroll.vue
+++ client/src/views/home/Scroll.vue
@@ -1,52 +1,85 @@
 <template>
-  <div>
-    <img
-      v-for="(picture, index) of pictures"
-      :key="picture.src"
-      v-media="picture.src"
-      :style="{
-        left: `${(0.6 + 0.4 * picture.speed) * (50 * index - scrolled)}vw`,
-        top: `${30 + 40 * picture.y}vh`,
-      }"
-    />
+  <div class="outer" ref="outer">
+    <div class="inner">
+      <img
+        v-for="picture of pictures"
+        :key="picture.src"
+        v-media="picture.src"
+        :style="{
+          left: `${25 + 25 * picture.x}%`,
+          top: `${50 * picture.y}%`,
+        }"
+      />
+      <img
+        v-for="picture of pictures"
+        :key="picture.src"
+        v-media="picture.src"
+        :style="{
+          left: `${25 + 25 * picture.x}%`,
+          top: `${50 + 50 * picture.y}%`,
+        }"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import srcs from "../../content/home.json";
 
-const pictures = srcs.map((src) => ({
-  y: Math.random(),
-  speed: Math.random(),
+const pictures = srcs.map((src, i) => ({
   src,
+  x: Math.random(),
+  y: (i + Math.random()) / srcs.length,
 }));
 
 export default {
   data() {
     return {
       pictures,
-      scrolled: 0,
     };
   },
   mounted() {
-    window.addEventListener("wheel", this.onWheel);
+    const { outer } = this.$refs;
+    outer.addEventListener("scroll", this.onScroll);
+    console.log(outer.scrollTop, (outer.scrollHeight - window.innerHeight) / 2);
+    outer.scrollTop = (outer.scrollHeight - window.innerHeight) / 2;
+    console.log(outer.scrollTop);
   },
   beforeDestroy() {
-    window.removeEventListener("wheel", this.onwheel);
+    this.$refts.outer.removeEventListener("scroll", this.onScroll);
   },
   methods: {
-    onWheel(evt) {
-      this.scrolled += 0.1 * evt.deltaY;
+    onScroll() {
+      const { outer } = this.$refs;
+      if (outer.scrollTop / (outer.scrollHeight - window.innerHeight) < 0.25) {
+        outer.scrollTop = outer.scrollTop + 0.5 * outer.scrollHeight;
+      }
+      if (outer.scrollTop / (outer.scrollHeight - window.innerHeight) > 0.75) {
+        outer.scrollTop = outer.scrollTop - 0.5 * outer.scrollHeight;
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.outer {
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+}
+.inner {
+  width: 100vh;
+  height: 5000vh;
+  position: relative;
+}
 img {
-  position: fixed;
+  position: absolute;
   pointer-events: none;
   height: 45vh;
   transform: translate(-50%, -50%);
+  max-height: 50vh;
+  max-width: 50vw;
+  object-fit: contain;
 }
 </style>
